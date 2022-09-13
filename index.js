33822cfb8c7285143d176472d20e7f258162a905
Adarsh Kumar Srivastava <adarshman98@gmail.com>
2022-09-13 15:29:11 +0530

    final commit app completed

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const io = require('socket.io')(3000 , { cors: { origin: '*' } })
+const io = require("socket.io")(3000,{ cors: { origin: '*' } })
 
-io.on("connection" ,(socket)=>{
-    console.log("new user there")
-    socket.on("user",(name)=>{
-        console.log(name)
-        socket.broadcast.emit("new-user",name)
-    })
+var users = {};
+
+io.on("connection" , socket =>{
+  console.log("connection established")
+  socket.on("user-joined",name =>{
+    console.log(name)
+    users[socket.id] = name;
+    console.log(name)
+    socket.broadcast.emit('new-user', name)
+  })
+
+  socket.on("new-message" , (data)=>{
+    socket.broadcast.emit("message", data)
+  })
 })
--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-var socket = io('http://localhost:3000')
-const n = prompt("enter a user name")
-socket.emit("user",n)
+const socket  = io('http://localhost:3000')
+const n =  prompt("enter your name")
+
+socket.emit("user-joined" ,n)
+function sendMessage(){
+    console.log("hello moto")
+    
+    let value  =  document.getElementById("message").value
+    data = {name : n , message : value}
+    const para = document.createElement("p");
+    para.innerText = "You" + ":" + value;
+    document.body.appendChild(para);
+
+    socket.emit("new-message" ,data)
+}
+socket.on("new-user",(name)=>{
+
+    document.getElementById("text-1").innerHTML = name + " join the chat";
+})
+socket.on("message",(data)=>{
+    const para = document.createElement("p");
+    para.innerText = data.name + ":" + data.message;
+    document.body.appendChild(para);
+      
+})
